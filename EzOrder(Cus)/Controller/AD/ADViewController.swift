b201cc481e3b7b6be5be5f10dda749c2f1278c91
Ken <ken_60401@yahoo.com.tw>
2019-05-19 23:33:33 +0800

    1.加入側滑ScrollView主廣告牆

--- EzOrder(Cus)/Controller/AD/ADViewController.swift
+++ EzOrder(Cus)/Controller/AD/ADViewController.swift
@@ -5,16 +5,44 @@
 //  Created by 李泰儀 on 2019/5/15.
 //  Copyright © 2019 TerryLee. All rights reserved.
 //
-
+// FACCCA
 import UIKit
 
-class ADViewController: UIViewController {
-
+class ADViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollviewAd: UIScrollView!
+    @IBOutlet weak var pgcontrolAd: UIPageControl!
+    @IBOutlet weak var imgAd1: UIImageView!
+    @IBOutlet weak var imgAd2: UIImageView!
+    @IBOutlet weak var imgAd3: UIImageView!
+    @IBOutlet weak var imgAd4: UIImageView!
+    @IBOutlet weak var imgAd5: UIImageView!
+    
+    @IBOutlet weak var collectviewUpper: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgAd1.image = UIImage(named: "AD1")
+        imgAd2.image = UIImage(named: "AD2")
+        imgAd3.image = UIImage(named: "AD3")
+        imgAd4.image = UIImage(named: "AD4")
+        imgAd5.image = UIImage(named: "AD5")
+        
+        
+
+        scrollviewAd.delegate = self
+        scrollviewAd.showsVerticalScrollIndicator = false
+        scrollviewAd.showsHorizontalScrollIndicator = false
+        scrollviewAd.scrollsToTop = false
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        //通过scrollView内容的偏移计算当前显示的是第几页
+        let page = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        //设置pageController的当前页
+        pgcontrolAd.currentPage = page
+    }
+    
 
 }
 
---
+++ EzOrder(Cus)/Controller/AD/UpperCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UpperCollectionViewCell.swift
+//  EzOrder(Cus)
+//
+//  Created by Lee Chien Kuan on 2019/5/19.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import UIKit
+
+class UpperCollectionViewCell: UICollectionViewCell {
+    
+}
